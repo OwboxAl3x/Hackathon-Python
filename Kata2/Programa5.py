06cc3e002df6bf73064bef084a4e8fdeceaf4272
Alejandro García <agarciavallecillo@gmail.com>
2020-05-16 15:55:35 +0200

    Mejoras

--- Kata2/Programa5.py
+++ Kata2/Programa5.py
@@ -1,17 +1,17 @@
 # Vamos a aprender qué es un While
 
 password = "1234"
-intentosMax = 5
+INTENTOS_MAX = 5
 intento = 1
 
 passwordIntroducida = input("\nIntroduzca su contraseña: ")
 
-while password != passwordIntroducida and intento < intentosMax:
-    passwordIntroducida = input("\nContraseña erronea, prueba de nuevo (quedan " + str(intentosMax - intento) + " intentos): ")
+while password != passwordIntroducida and intento < INTENTOS_MAX:
+    passwordIntroducida = input("\nContraseña erronea, prueba de nuevo (quedan " + str(INTENTOS_MAX - intento) + " intentos): ")
     intento = intento + 1
 
 
-if  intento == intentosMax:
+if  intento == INTENTOS_MAX:
     print("\nHas superado los intentos permitidos.\n")
 else:
     print("\nContraseña correcta.\n")
